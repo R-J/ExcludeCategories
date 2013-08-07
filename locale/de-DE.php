10f169bee38e1a89fc5b47c8542bfcbb2bd30e7b
R-J <robin.jurinka@gmx.net>
2013-08-07 16:31:18 +0200

    Create de-DE.php

---
+++ locale/de-DE.php
@@ -0,0 +1,6 @@
+<?php if(!defined('APPLICATION')) exit();
+
+$Definition['Check any of the categories below to hide the discussions in that category from discussion view.'] = 'Markiere die Kategorien, deren Diskussionen in der Diskussions-Übersicht nicht angezeigt werden sollen';
+$Definition['Save'] = 'Speichern';
+$Definition['Saved'] = 'Gespeichert';
+$Definition['Exclude Categories Settings'] = 'Exclude Categories Einstellungen';
